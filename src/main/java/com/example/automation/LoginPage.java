f0fccfe006447571f06232b682ca2b0f8734a18e
Aditi Desai <132979412+githubcodingjoiner@users.noreply.github.com>
2025-01-03 14:25:40 +0530

    Update LoginPage.java

--- src/main/java/com/example/automation/LoginPage.java
+++ src/main/java/com/example/automation/LoginPage.java
@@ -1,43 +1,29 @@
 package com.example.automation;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.jupiter.api.Test;
+public class LoginPage {
+    private String username;
+    private String password;
+    private boolean isLoggedIn;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+    public LoginPage() {
+        this.isLoggedIn = false;
+    }
 
-public class LoginAutomationTest {
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-    @Test
-    public void testLogin() {
-        // Set up the WebDriver
-        System.setProperty("webdriver.chrome.driver", "C://Program Files//chromedriver//chromedriver");
-        WebDriver driver = new ChromeDriver();
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-        try {
-            // Navigate to the login page
-            driver.get("https://the-internet.herokuapp.com/login");
-
-            // Locate the username, password fields, and login button
-            WebElement usernameField = driver.findElement(By.id("username"));
-            WebElement passwordField = driver.findElement(By.id("password"));
-            WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
-
-            // Perform login
-            usernameField.sendKeys("tomsmith");
-            passwordField.sendKeys("SuperSecretPassword!");
-            loginButton.click();
-
-            // Validate successful login
-            WebElement successMessage = driver.findElement(By.id("flash"));
-            assertTrue(successMessage.getText().contains("You logged into a secure area!"),
-                    "Login success message not found or incorrect!");
-
-        } finally {
-            // Close the browser
-            driver.quit();
+    public void clickLogin() {
+        if ("testuser".equals(username) && "password123".equals(password)) {
+            isLoggedIn = true;
         }
+    }
+
+    public boolean isLoggedIn() {
+        return isLoggedIn;
     }
 }
